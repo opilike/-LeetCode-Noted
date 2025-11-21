6ecd0784ba6b50fd62262188f8a0160a1d054f23
MingYiLin <ps211psp@gmail.com>
2025-11-21 23:21:28 +0800

    Improve comments in Merge k Sorted Lists implementation
    Refactor comments for clarity and understanding of the algorithm.

--- 23. Merge k Sorted Lists.js
+++ 23. Merge k Sorted Lists.js
@@ -16,20 +16,35 @@ class Solution {
         if(lists == null){
             return null;
         }
-        // 這是輸入的陣列：ListNode[] lists
+        // 把每一項lists裝進去，
+        // list1 1 -> 1 -> 3
+        // list2 2 -> 2 -> 5
+        // pq跑完會是 [1] [2]
         for (ListNode list : lists) {
             if(list != null){
                 pq.add(list);
             }
         }
+        
         while (!pq.isEmpty()) {
-            ListNode smallest = pq.poll();
-            cur.next = smallest;
-            cur = cur.next;
-            // 在 cur = cur.next; 之後：
+            ListNode smallest = pq.poll();   //接到pq裡最小的(最有權重的)
+            cur.next = smallest;             //指針只到smallest
+            cur = cur.next;                  //指針前進下一步
+            // 如果list的下一項不為空，我們就在抓近來
+            // 例如已經做完 dummyhead 1 -> 
+            // 我們會再去抓 1 -> 1 ->
             if (smallest.next != null) {
                 pq.add(smallest.next);
             }
         }return dummyhead.next;
     }
 }
+
+/**
+把一整串丟進來
+但只會抓第一項目
+再透過判斷目前PQ裡面裝的東西，
+做指標的移動位移
+然後假設list1 還沒抓完我們會一直抓近來比較
+然後一直彈出最小的值一值串下去直到都串完
+**/
