e96526d0025f54bcca66742abdff1d55b632fde0
MingYiLin <ps211psp@gmail.com>
2025-11-23 00:45:04 +0800

    Add method to delete the middle node of a linked list
    Implement fast and slow pointer technique to delete the middle node from a linked list.

--- 2095. Delete the Middle Node of a Linked List.java
+++ 2095. Delete the Middle Node of a Linked List.java
@@ -53,3 +53,35 @@ cur 是複製一串一模一樣的head
   **//
   結論： cur 不是複製一串一模一樣的串列。cur 和 head 只是兩個不同的名字，指向同一個節點。因此，當您透過 cur 執行 cur.next = cur.next.next; 時，您是直接修改了 head 指向的那個鏈結串列結構。這就是為什麼您最後返回 head 時，結構的改變會被保留下來。
 
+
+快慢指標作法
+    ----------------------------------------------------------------------------------------------
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if(head == null || head.next == null){
+            return null;
+        }
+        //快慢指標 slow在dummy, fast在head上
+        ListNode dummy = new ListNode(-1);
+        ListNode slow = dummy;
+        slow.next = head;
+        ListNode fast = head;
+        // 讓s停在該刪除的在前一位
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }slow.next = slow.next.next;
+        return head;
+    }
+
+    }
